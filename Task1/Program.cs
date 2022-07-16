8b4722d744b03cc1100054a074483d6525de14f3
lubov <loveavduhova@gmail.com>
2022-07-16 11:30:33 +0700

    homework done

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*  Задача 1: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+int Prompt (string message)
+{
+Console.Write (message);
+int a = int.Parse (Console.ReadLine ());
+return a;
+}
+
+int number = Prompt ("Введите число ");
+int raised = Prompt ("Введите степень числа ");
+int result = 1;
+
+
+for (int i = 0; i < raised; i++)
+{
+    result = number * result;
+}
+System.Console.WriteLine(result);
---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*  Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+int Prompt (string message)
+{
+Console.Write (message);
+int a = int.Parse (Console.ReadLine ());
+return a;
+}
+
+
+int number = Prompt ("Введите число ");
+int sum = 0;
+int result = 0;
+
+for (int i = 1; i <= number; i++)
+{
+    result = number % 10;
+    number = number / 10;
+    sum = result + sum;
+}
+
+    Console.WriteLine(sum);
+
